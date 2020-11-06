bbc621dc248ddfbabdfc944047383f3390f1a512
jiayoucao <1037887028@qq.com>
2020-11-06 21:58:14 +0800

    LeetCode 83. 删除排序链表中的重复元素  递归实现

---
+++ deleteDuplicates.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+/**
+83. 删除排序链表中的重复元素
+给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+
+示例 1:
+
+输入 : 1->1->2
+输出 : 1->2
+	 示例 2 :
+
+ 输入 : 1->1->2->3->3
+  输出 : 1->2->3
+	  
+	   * Definition for singly-linked list.
+	   * struct ListNode {
+	   *     int val;
+	   *     struct ListNode *next;
+	   * };
+	   */
+
+//递归方法实现
+
+	 struct ListNode* deleteDuplicates(struct ListNode* head){
+		 if (!head || !head->next)
+			 return head;
+		 head->next = deleteDuplicates(head->next);
+		 if (head->val == head->next->val)
+		 {
+			 head = head->next;
+
+		 }
+		 return head;
+
+	 }
