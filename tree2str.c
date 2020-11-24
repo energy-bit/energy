05511f73e5f27dece7392a787fce8ec59aa991cd
jiayoucao <1037887028@qq.com>
2020-11-24 17:34:00 +0800

    LeetCode  606. 根据二叉树创建字符串

---
+++ tree2str.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+/*606. 根据二叉树创建字符串
+你需要采用前序遍历的方式，将一个二叉树转换成一个由括号和整数组成的字符串。
+
+空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
+
+示例 1:
+
+输入: 二叉树: [1,2,3,4]
+       1
+     /   \
+    2     3
+   /    
+  4     
+
+输出: "1(2(4))(3)"
+
+解释: 原本将是“1(2(4)())(3())”，
+在你省略所有不必要的空括号对之后，
+它将是“1(2(4))(3)”。
+示例 2:
+
+输入: 二叉树: [1,2,3,null,4]
+       1
+     /   \
+    2     3
+     \  
+      4 
+
+输出: "1(2()(4))(3)"
+
+解释: 和第一个示例相似，
+除了我们不能省略第一个对括号来中断输入和输出之间的一对一映射关系。*/
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     struct TreeNode *left;
+*     struct TreeNode *right;
+* };
+*/
+
+void _tree2str(struct TreeNode* t, char *str)
+{
+	if (t == NULL)
+		return;
+	char buf[10] = { 0 };
+	sprintf(buf, "%d", t->val);
+	strcat(str, buf);
+	if (t->left == NULL)
+	{
+		if (t->right != NULL)
+			strcat(str, "()");
+		else
+			return;
+	}
+	else
+	{
+		strcat(str, "(");
+		_tree2str(t->left, str);
+		strcat(str, ")");
+	}
+	if (t->right == NULL)
+		return;
+	else
+	{
+		strcat(str, "(");
+		_tree2str(t->right, str);
+		strcat(str, ")");
+	}
+}
+char * tree2str(struct TreeNode* t)
+{
+	int n = 100000;
+	char *str = (char*)malloc(sizeof(char)*n);
+	memset(str, 0, n);
+	_tree2str(t, str);
+	return str;
+}
