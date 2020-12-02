c13d5236be693e0917230d5ba8c7f4f60f0159e7
jiayoucao <1037887028@qq.com>
2020-12-02 18:10:18 +0800

    Leetcode  142. 环形链表 II  快慢指针

---
+++ detectCycle.c
@@ -0,0 +1,57 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+/*给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 
+如果 pos 是 -1，则在该链表中没有环。
+注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+
+。*/
+
+/*输入：head = [3,2,0,-4], pos = 1
+输出：返回索引为 1 的链表节点
+解释：链表中有一个环，其尾部连接到第二个节点。
+
+输入：head = [1,2], pos = 0
+输出：返回索引为 0 的链表节点
+解释：链表中有一个环，其尾部连接到第一个节点。
+
+输入：head = [1], pos = -1
+输出：返回 null
+解释：链表中没有环。*/
+
+
+//   解法   用快慢指针  慢指针走一步   快指针走两步   如果有环  快指针走一圈就会和慢指针重合  证明有环  负责无环
+
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     struct ListNode *next;
+* };
+*/
+struct ListNode *detectCycle(struct ListNode *head)
+{
+	if (head == NULL)
+		return NULL;
+	struct ListNode *fast, *slow;
+	fast = slow = head;
+	while (fast != NULL&&fast->next != NULL)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+			break;
+	}
+	if (fast == NULL || fast->next == NULL)//   要注意只有一个结点   不为环
+		return NULL;
+	fast = head;
+	while (fast != slow)
+	{
+		fast = fast->next;
+		slow = slow->next;
+	}
+	return slow;
+}
+
+
